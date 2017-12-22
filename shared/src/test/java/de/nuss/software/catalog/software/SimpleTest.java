36ae1263147a405bb7262cd0a66943ce5942a2d6
tnus <thorsten.nuss@bilfinger.com>
2017-12-22 08:58:18 +0100

    added test aggregation

---
+++ shared/src/test/java/de/nuss/software/catalog/software/SimpleTest.java
@@ -0,0 +1,17 @@
+package de.nuss.software.catalog.software;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		// OK
+	}
+
+}
